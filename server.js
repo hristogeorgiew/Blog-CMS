d998678e068974c7ee9d6e569df42074fa501c3c
Hristo <36264387+hristogeorgiew@users.noreply.github.com>
2021-03-21 12:15:50 +0200

    setup server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello Blog');
+})
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}` );
+});
