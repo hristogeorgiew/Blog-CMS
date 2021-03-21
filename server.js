f89e8c3cd5029632eef2b2d7d880da286de8c177
Hristo <36264387+hristogeorgiew@users.noreply.github.com>
2021-03-21 13:02:17 +0200

    Connect Database (MongoDB Atlas)

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connect = require('./config/db');
 require('dotenv').config();
 const app = express();
+
+//connect mongoDB database
+connect();
+
 
 app.get('/', (req, res) => {
     res.send('Hello Blog');
