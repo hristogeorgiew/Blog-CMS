22b5bbd2a90821980491baff4f4ebc115d76724c
Hristo <36264387+hristogeorgiew@users.noreply.github.com>
2021-03-21 15:50:27 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const app = require('express');
 const router = app.Router();
 const { register, registerValidations } = require('../controllers/userControler');
 
+//use middleware
 router.post("/register", registerValidations, register);
 
 module.exports = router; 
