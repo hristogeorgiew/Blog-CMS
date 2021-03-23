8552ce9d854b5c35ac9d59bf0472a4ebc32f57f3
Hristo <h.georgiew93@gmail.com>
2021-03-23 10:15:36 +0200

    Add state, setState
    data collection

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import BgImage from "./BgImage";
 import { Helmet } from "react-helmet";
+import { useDispatch } from "react-redux";
 
 const Register = () => {
+
+    const [state, setState] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
+
+    const useDispatch();
+
+    const handleInputs = e => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const userRegister = e => {
+        e.preventDefault();
+        console.log(state);
+    };
+
     return (
         <>
             <Helmet>
@@ -15,18 +38,39 @@ const Register = () => {
                 <div className="col-4">
                     <div className="account">
                         <div className="account_section">
-                            <form>
+
+                            <form onSubmit={userRegister}>
                             <div className="group">
                                     <h3 className="form-heading">Register</h3>
                                 </div>
                                 <div className="group">
-                                    <input type="text" name="" className="group_control" placeholder="Enter Name"/>
+                                    <input 
+                                        type="text" 
+                                        name="name" 
+                                        className="group_control" 
+                                        placeholder="Enter Name"
+                                        value={state.name}
+                                        onChange={handleInputs}
+                                    />
                                 </div>
                                 <div className="group">
-                                    <input type="email" name="" className="group_control" placeholder="Enter Email"/>
+                                    <input 
+                                        type="email" 
+                                        name="email" 
+                                        className="group_control" 
+                                        placeholder="Enter Email"
+                                        value={state.email}
+                                        onChange={handleInputs}
+                                    />
                                 </div>
                                 <div className="group">
-                                    <input type="password" name="" className="group_control" placeholder="Create Password"/>
+                                    <input 
+                                        type="password" 
+                                        name="password" className="group_control" 
+                                        placeholder="Create Password"
+                                        value={state.password}
+                                        onChange={handleInputs}
+                                    />
                                 </div>
                                 <div className="group">
                                     <input type="submit" name="" className="btn btn-default btn-block" value="Register"/>
